7b8405b7676bef5a046e8de7c23e984fedd31fbe
JohnAndante <walker_contas@hotmail.com>
2024-05-11 18:04:34 -0300

    Update Novo Módulo

---
+++ estruturas-condicionais-java/src/Aula01.java
@@ -0,0 +1,11 @@
+public class Aula01 {
+    /*
+     * Controle de fluxo
+     * Habilidade de ajustar a maneira como um programa realiza suas tarefas
+     *
+     * Classificação
+     * Estruturas condicionais  -   if, else, switch
+     * Estruturas de repetição  -   for, while, do-while
+     * Estruturas de exceções   -   try, catch, finally, throw, throws
+     */
+}
